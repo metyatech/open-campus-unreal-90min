27d05df5759320e1880992c12b359c18c6198157
metyatech <metyatech@gmail.com>
2026-04-14 17:38:31 +0900

    3-3 まで見直し

--- content/docs/_meta.ts
+++ content/docs/_meta.ts
@@ -2,14 +2,6 @@ const meta = {
   intro: {},
   'lesson-overview': {},
   'student-guide': {},
-  'teacher-guide': {
-    title: '教員ガイド',
-    display: 'hidden',
-  },
-  'setup-and-troubleshooting': {
-    title: 'セットアップ・トラブル対応',
-    display: 'hidden',
-  },
 };
 
 export default meta;
